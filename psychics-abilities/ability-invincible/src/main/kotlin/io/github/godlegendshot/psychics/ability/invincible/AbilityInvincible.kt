e22f2c9dd4b3b4ee271472bc264ddd71f0f7fb16
legendshot <legendshot@naver.com>
2022-10-08 19:47:06 +0900

    부매랑 복구(완전치는 않음) 및 기타 추가

---
+++ psychics-abilities/ability-invincible/src/main/kotlin/io/github/godlegendshot/psychics/ability/invincible/AbilityInvincible.kt
@@ -0,0 +1,40 @@
+package io.github.godlegendshot.psychics.ability.invincible
+
+import io.github.monun.psychics.Ability
+import io.github.monun.psychics.AbilityConcept
+import io.github.monun.tap.config.Name
+import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.player.PlayerVelocityEvent
+
+//project-invincible
+@Name("invincible")
+class AbilityConceptInvincible : AbilityConcept() {
+    init {
+        displayName = "무적"
+        description = listOf(
+            text("무적의 힘으로 데미지를 입지 않는다."),
+            text("절★대★무★적").color(NamedTextColor.YELLOW)
+        )
+    }
+}
+
+class  AbilityInvincible : Ability<AbilityConceptInvincible>() ,Listener {
+    override fun onEnable() {
+        psychic.registerEvents(this)
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onEntityDamage(event: EntityDamageEvent) {
+        val damage = event.damage * 0
+        event.damage = damage
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    fun onVelocity(event: PlayerVelocityEvent) {
+        event.isCancelled = true
+    }
+}
